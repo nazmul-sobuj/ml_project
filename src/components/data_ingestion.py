065578b1f17aa92d5b81754ccab67f6f2be5ebd1
nazmul-sobuj <sobujiiuc73@gmail.com>
2025-04-28 14:08:44 +0600

    eda part

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,36 +7,39 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 @dataclass
-class DataIngestinConfig:
+class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
     test_data_path: str = os.path.join('artifacts', 'test.csv')
     raw_data_path: str = os.path.join('artifacts', 'raw.csv')
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config = DataIngestinConfig()
+        self.ingestion_config = DataIngestionConfig()
 
-    def initiate_data_config(self):
-        logging.info("Entered the data ingestion method or componenets")
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or components")
         try:
-            df = pd.read_csv('notebook\data\student.csv')
-            logging.info("Read the dataset as Dataframe")
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok= True)
-            df.to_csv(self.ingestion_config.raw_data_path, index= False, header= True)
-            logging.info('Train test split initiate')
-            train_set, test_set = train_test_split(df, test_size = 0.2, random_state = 101)
-            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+            df = pd.read_csv(os.path.join('notebook', 'data', 'student.csv'))
+            logging.info("Read the dataset as DataFrame")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info('Train-test split initiated')
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=101)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
             logging.info('Ingestion of data is completed')
-            return(
+
+            return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
+            logging.error(f"Exception occurred: {e}")
             raise CustomException(e, sys)
-        
 
-if __name__== "__main__":
+if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_config()
-      
+    obj.initiate_data_ingestion()
--- src/exception.py
+++ src/exception.py
@@ -1,22 +1,28 @@
 import sys
 from src.logger import logging
 
-def error_message_detail(error, error_detail:sys):
+def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
-    filename = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in the script filename [{0}] line no [{1}] error message[{2}]".format(
-        filename, exc_tb.tb_lineno, str(error)
-    )
+    
+    if exc_tb is not None:
+        filename = exc_tb.tb_frame.f_code.co_filename
+        error_message = "Error occurred in the script [{0}] at line [{1}]: {2}".format(
+            filename, exc_tb.tb_lineno, str(error)
+        )
+    else:
+        error_message = f"Error occurred: {str(error)}"
+    
     return error_message
 
-
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail= error_detail)
+    def __init__(self, error_message, error_detail: sys):
+        self.error_message = error_message_detail(error_message, error_detail)
+        logging.error(self.error_message)  # Optional: log when error is created
+        super().__init__(self.error_message)
 
     def __str__(self):
         return self.error_message
+
     
 
 
